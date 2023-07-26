fac164d0517e1f792087eb74dba4600001c37fe9
Yehia <yehia.essa2030@gmail.com>
2023-07-26 14:37:23 +0300

    first commit

---
+++ app/src/main/java/com/example/training/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.training
+
+import android.os.Bundle
+import android.util.Log
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
