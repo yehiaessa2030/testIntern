48a60ba1b31be9dafde95198660538f7c033d6b2
Yehia <yehia.essa2030@gmail.com>
2023-07-26 15:04:56 +0300

    main activity has been updated with print

--- app/src/main/java/com/example/training/MainActivity.kt
+++ app/src/main/java/com/example/training/MainActivity.kt
@@ -12,8 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         print("dsafdsafdsfasf")
-        println("tasdafdsafdsafdsafdsa")
-
+        print("dsafdsafdsafdsaf")
 
 
     }
